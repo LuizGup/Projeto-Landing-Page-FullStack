6ac87fbee15276634cabd358757ec76834c730c1
LuizGup <117296493+LuizGup@users.noreply.github.com>
2024-03-22 00:04:05 -0300

    added morer things

---
+++ index.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {
+    var navbar = document.getElementById("navbar");
+    if (window.scrollY > 30) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  };
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var observer = new IntersectionObserver(function(entries) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fadeInUp');
+        }
+      });
+    });
+  
+    document.querySelectorAll('.animatable').forEach(img => {
+      observer.observe(img);
+    });
+  });
